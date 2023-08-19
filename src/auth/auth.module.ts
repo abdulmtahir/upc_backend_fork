b721ca13d0db2ae682c3222247009731a5021e4e
Hamza Muhammad Idris <109629450+HamzaMuhd@users.noreply.github.com>
2023-08-19 22:31:08 +0100

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -18,7 +18,7 @@ import { LocalAuthGuard } from './guards/local.guard';
     JwtModule.register({
       global: true,
       secret: jwtConstant.secret,
-      signOptions: { expiresIn: '3600000s' },
+      signOptions: { expiresIn: '5m' },
     }),
   ],
   controllers: [AuthController],

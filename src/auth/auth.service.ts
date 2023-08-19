bbbb6a01f4b7f47d374ec2a1730bc01cb4b1a1aa
Hamza Muhammad Idris <109629450+HamzaMuhd@users.noreply.github.com>
2023-08-19 19:56:09 +0100

    Update auth.service.ts
    Added username to jwt token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
   }
 
   async login(user: Admin) {
-    const payload = { email: user.email, id: user.id, role: user.role };
+    const payload = { email: user.email, id: user.id, role: user.role, username: user.first_name };
     return {
       access_token: this.jwtService.sign(payload),
     };
